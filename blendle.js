d7927729b2296c1bf5a9410650ae2e8cbb06c7a6
ronkeizer <ronkeizer@gmail.com>
2015-01-08 08:15:25 -0800

    fix bug date

--- blendle.js
+++ blendle.js
@@ -1,12 +1,13 @@
 var inp = location.href.split(/\//);
 var j = inp[2].replace(/(www\.|\.nl|\.com)/g, "");
 var url;
+function add_z(n) { return n<10? '0'+n:''+n; }
 if (j === "nrc") {
     var v = inp[3];
     var vers = "nrc";
     if (v === "next") { vers = "nn"};
     var m = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
-    var dat = inp[5]+(m.indexOf(inp[6])+1)+inp[7];
+    var dat = inp[5]+add_z((m.indexOf(inp[6])+1))+add_z(inp[7]);
     var art = inp.pop().split("-");
     var num = art.pop();
     url = "https://blendle.nl/i/nrc-handelsblad/"+art.join("-")+"/bnl-"+vers+"-"+dat+"-"+num;
